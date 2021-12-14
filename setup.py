88247c60563d37be5cddd4c638f94cca24ec149a
joshua <pitt.joshua.miller@gmail.com>
2021-12-14 10:34:03 -0500

    Park helium-py on pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='helium-py',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='',
+    author='Crypto Balloon LLC',
+    author_email='joshua@cryptoballoon.net',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
