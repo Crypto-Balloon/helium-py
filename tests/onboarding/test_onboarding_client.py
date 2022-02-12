e7e7ac26c47685ac535291e1f7054cb1b2b0f180
Joe Cronyn <joe@Joes-Air.localdomain>
2022-02-11 21:06:54 -0500

    fixing test typo

--- tests/onboarding/test_onboarding_client.py
+++ tests/onboarding/test_onboarding_client.py
@@ -8,7 +8,7 @@ def test_client_integration():
     """Initial integration tests for Onboarding Client."""
     client = OnboardingClient()
     assert 'maker' in client.get_onboarding_record(some_hotspot)
-    assert 'locationNoneLimit' in client.get_makers()[0]
+    assert 'locationNonceLimit' in client.get_makers()[0]
     assert 'version' in client.get_firmware()
     # TODO: Test Payment Transaction
     # client.post_payment_transaction("some address", "some_txn_hash")
