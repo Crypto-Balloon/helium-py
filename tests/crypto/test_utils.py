bcbe382e3642cbc98f5467e30bd1a49aa49baa9f
joshua <joshua@cryptoballoon.net>
2022-02-19 18:16:01 -0500

    Move users fixture module into a pytest fixture

--- tests/crypto/test_utils.py
+++ tests/crypto/test_utils.py
@@ -6,7 +6,6 @@ import base58
 from helium_py.crypto import utils
 from helium_py.crypto.address import Address
 from helium_py.crypto.constants import NetTypes
-from tests.crypto.fixtures import bob, bobB58
 
 MAINNET = NetTypes.MAINNET.value
 
@@ -18,18 +17,18 @@ def test_derive_checksum_bits():
     assert derivedChecksumBits == '0011'
 
 
-def test_encode_publickey_to_bs58_address():
-    address = Address(Address.DEFAULT_VERSION, MAINNET, 1, bob.public_key)
+def test_encode_publickey_to_bs58_address(users):
+    address = Address(Address.DEFAULT_VERSION, MAINNET, 1, users.bob.keypair.public_key)
     versioned_payload = bytes([0]) + address.bin
     checksum = utils.sha256(binascii.unhexlify(utils.sha256(versioned_payload)))
     checksum_bytes = bytes(binascii.unhexlify(checksum[:8]))
     result = versioned_payload + checksum_bytes
     encoded = base58.b58encode(result)
-    assert encoded == bobB58 == utils.bs58_check_encode(0, address.bin)
+    assert encoded == users.bob.b58 == utils.bs58_check_encode(0, address.bin)
 
 
-def test_bs58_to_bin():
-    address_b58 = Address(Address.DEFAULT_VERSION, MAINNET, 1, bob.public_key).b58
+def test_bs58_to_bin(users):
+    address_b58 = Address(Address.DEFAULT_VERSION, MAINNET, 1, users.bob.keypair.public_key).b58
     binary = base58.b58decode(address_b58)
     versioned_payload = binary[0:-4]
     payload = binary[1:-4]
