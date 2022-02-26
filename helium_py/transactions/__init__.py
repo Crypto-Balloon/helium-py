3f386f0d2dd9c24d83200d56ccb803b4226f9217
Joe Cronyn <joe@Joes-Air.localdomain>
2022-02-25 22:55:22 -0500

    Adding PaymentV2

--- helium_py/transactions/__init__.py
+++ helium_py/transactions/__init__.py
@@ -4,5 +4,6 @@ from .add_gateway_v1 import AddGatewayV1
 from .assert_location_v1 import AssertLocationV1
 from .assert_location_v2 import AssertLocationV2
 from .payment_v1 import PaymentV1
+from .payment_v2 import PaymentV2
 
 __version__ = VERSION
--- helium_py/transactions/payment_v2.py
+++ helium_py/transactions/payment_v2.py
@@ -1,5 +1,7 @@
 """Replace Placeholder Docstring."""
+import base64
 import typing
+from dataclasses import dataclass
 from typing import Optional
 
 from helium_py import proto
@@ -9,48 +11,58 @@ from helium_py.transactions.transaction import Transaction
 from helium_py.transactions.utils import EMPTY_SIGNATURE
 
 
-class PaymentV1(Transaction):
+@dataclass
+class Payment:
     """Replace Placeholder Docstring."""
 
-    type: str = 'payment_v1'
+    payee: Address
+    amount: int
+    memo: Optional[bytes]
+
+
+class PaymentV2(Transaction):
+    """Replace Placeholder Docstring."""
+
+    type: str = 'payment_v2'
     payer: Optional[Address]
-    payee: Optional[Address]
-    amount: Optional[int] = None
+    payments: Optional[typing.List[Payment]]
     fee: Optional[int] = None
     nonce: Optional[int] = None
-
     signature: Optional[bytes]
 
     def __init__(
             self,
             payer: Optional[Address],
-            payee: Optional[Address],
-            amount: Optional[int] = None,
+            payments: Optional[typing.List[Payment]],
             fee: Optional[int] = None,
             nonce: Optional[int] = None,
+            signature: Optional[bytes] = None,
     ):
         """Replace Placeholder Docstring."""
         self.payer = payer
-        self.payee = payee
-        self.amount = amount
-        self.fee = fee if fee is not None else self.calculate_fee()
+        self.payments = payments if payments else []
         self.nonce = nonce
+        self.fee = fee if fee is not None else self.calculate_fee()
+        self.signature = signature
 
     @classmethod
-    def deserialize(cls, serialized_transaction: bytes) -> 'PaymentV1':
+    def deserialize(cls, serialized_transaction: bytes) -> 'PaymentV2':
         """Replace Placeholder Docstring."""
-        payment_proto = proto.BlockchainTxn.FromString(serialized_transaction).payment
+        payment_proto = proto.BlockchainTxn.FromString(serialized_transaction).payment_v2
 
         payer = Address.from_bin(payment_proto.payer) if payment_proto.payer else None
-        payee = Address.from_bin(payment_proto.payee) if payment_proto.payee else None
-        amount = payment_proto.amount
+        payments = [
+            Payment(
+                payee=Address.from_bin(payment.payee),
+                amount=payment.amount,
+                memo=base64.b64encode(bytes(payment.memo.to_bytes(64, 'little', signed=False))))
+            for payment in payment_proto.payments]
         fee = payment_proto.fee
         nonce = payment_proto.nonce
 
         return cls(
             payer=payer,
-            payee=payee,
-            amount=amount,
+            payments=payments,
             fee=fee,
             nonce=nonce,
         )
@@ -58,10 +70,16 @@ class PaymentV1(Transaction):
     @typing.no_type_check
     def to_proto(self, for_signing=False) -> proto.BlockchainTxn:
         """Replace Placeholder Docstring."""
-        return proto.BlockchainTxn(payment=proto.BlockchainTxnPaymentV1(
+        payments = [
+            proto.Payment(
+                payee=payment.payee.bin,
+                amount=payment.amount,
+                memo=int.from_bytes(base64.b64decode(payment.memo), byteorder='little', signed=False))
+            for payment in self.payments]
+
+        return proto.BlockchainTxn(payment_v2=proto.BlockchainTxnPaymentV2(
             payer=self.payer.bin if self.payer else None,
-            payee=self.payee.bin if self.payee else None,
-            amount=self.amount if self.amount else None,
+            payments=payments,
             fee=self.fee if self.fee and self.fee > 0 else None,
             nonce=self.nonce if self.nonce else None,
             signature=self.signature if self.signature and not for_signing else None
@@ -70,7 +88,7 @@ class PaymentV1(Transaction):
     def sign(
             self,
             payer: Optional[Keypair] = None,
-    ) -> 'PaymentV1':
+    ) -> 'PaymentV2':
         """Replace Placeholder Docstring."""
         if not payer:
             raise ValueError('Payer argument required for signing')
