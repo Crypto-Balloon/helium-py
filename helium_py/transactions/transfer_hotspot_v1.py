9efffedeaecd96c34416dcf6402620959c100b56
joshua <joshua@cryptoballoon.net>
2022-02-27 00:29:58 -0500

    TransferHotspotV1(NewTransaction)

---
+++ helium_py/transactions/transfer_hotspot_v1.py
@@ -0,0 +1,36 @@
+"""Replace Placeholder Docstring."""
+from helium_py import proto
+from helium_py.transactions.decorators import transaction_class
+from helium_py.transactions.transaction import NewTransaction
+
+
+@transaction_class
+class TransferHotspotV1(NewTransaction):
+    """Replace Placeholder Docstring."""
+
+    type: str = 'transfer_hotspot_v1'
+    proto_model_class = proto.BlockchainTxnTransferHotspotV1
+    proto_txn_field = 'transfer_hotspot'
+    fields = {
+        'addresses': (
+            'gateway',
+            'seller',
+            'buyer',
+        ),
+        'signatures': (
+            'seller_signature',
+            'buyer_signature',
+        ),
+        'integers': (
+            'amount_to_seller',
+            'fee',
+            'buyer_nonce',
+        ),
+    }
+    defaults = {
+        'fee': 'calculated_fee',
+    }
+    keypairs = {
+        'seller': 'seller_signature',
+        'buyer': 'buyer_signature',
+    }
