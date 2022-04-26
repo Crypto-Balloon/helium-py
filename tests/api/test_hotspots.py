92950d148dbb566b4de283e3f2b2a901b7e0eec0
Joe <joe@cryptoballoon.net>
2022-04-26 19:50:46 -0400

    More test coverage

---
+++ tests/api/test_hotspots.py
@@ -0,0 +1,162 @@
+"""Tests for Client."""
+from unittest import mock
+
+from requests import Response, Session
+
+from helium_py.api import HELIUM_API_DEFAULT_HOST, Hotspots
+
+tx_instance = Hotspots()
+base_path = Hotspots.base_path
+
+
+@mock.patch.object(Session, 'get')
+def test_all(mock_get):
+    """Test that client allows custom port."""
+    mock_response = mock.Mock(spec=Response)
+    mock_response.json.return_value = {'data': [{'some': 'data'}]}
+    mock_get.return_value = mock_response
+    response = tx_instance.all()
+    assert next(response) == {'some': 'data'}
+    mock_get.assert_called_once_with(f'https://{HELIUM_API_DEFAULT_HOST}:443/v1/{base_path}/', params={})
+
+
+@mock.patch.object(Session, 'get')
+def test_hotspot_for_address(mock_get):
+    """Test that client allows custom port."""
+    mock_response = mock.Mock(spec=Response)
+    mock_response.json.return_value = {'data': [{'some': 'data'}]}
+    mock_get.return_value = mock_response
+    response = tx_instance.hotspot_for_address('some_address')
+    assert response[0] == {'some': 'data'}
+    mock_get.assert_called_once_with(f'https://{HELIUM_API_DEFAULT_HOST}:443/v1/{base_path}/some_address/', params={})
+
+
+@mock.patch.object(Session, 'get')
+def test_hotspots_for_name(mock_get):
+    """Test that client allows custom port."""
+    mock_response = mock.Mock(spec=Response)
+    mock_response.json.return_value = {'data': [{'some': 'data'}]}
+    mock_get.return_value = mock_response
+    response = tx_instance.hotspots_for_name('some_name')
+    assert response[0] == {'some': 'data'}
+    mock_get.assert_called_once_with(f'https://{HELIUM_API_DEFAULT_HOST}:443/v1/{base_path}/name/some_name/', params={})
+
+
+@mock.patch.object(Session, 'get')
+def test_hotspots_search_by_name(mock_get):
+    """Test that client allows custom port."""
+    mock_response = mock.Mock(spec=Response)
+    mock_response.json.return_value = {'data': [{'some': 'data'}]}
+    mock_get.return_value = mock_response
+    response = tx_instance.hotspots_search_by_name('some_name')
+    assert response[0] == {'some': 'data'}
+    mock_get.assert_called_once_with(f'https://{HELIUM_API_DEFAULT_HOST}:443/v1/{base_path}/name/',
+                                     params={'search': 'some_name'})
+
+
+@mock.patch.object(Session, 'get')
+def test_hotspots_search_by_location_distance(mock_get):
+    """Test that client allows custom port."""
+    mock_response = mock.Mock(spec=Response)
+    mock_response.json.return_value = {'data': [{'some': 'data'}]}
+    mock_get.return_value = mock_response
+    response = tx_instance.hotspots_search_by_location_distance(1, 2, 3)
+    assert response[0] == {'some': 'data'}
+    mock_get.assert_called_once_with(f'https://{HELIUM_API_DEFAULT_HOST}:443/v1/{base_path}/location/distance/',
+                                     params={'lat': 1, 'lon': 2, 'distance': 3})
+
+
+@mock.patch.object(Session, 'get')
+def test_hotspots_search_by_geo(mock_get):
+    """Test that client allows custom port."""
+    mock_response = mock.Mock(spec=Response)
+    mock_response.json.return_value = {'data': [{'some': 'data'}]}
+    mock_get.return_value = mock_response
+    response = tx_instance.hotspots_search_by_geo(1, 2, 3, 4)
+    assert response[0] == {'some': 'data'}
+    mock_get.assert_called_once_with(f'https://{HELIUM_API_DEFAULT_HOST}:443/v1/{base_path}/location/box/',
+                                     params={'swlat': 1, 'swlon': 2, 'nelat': 3, 'nelon': 4})
+
+
+@mock.patch.object(Session, 'get')
+def test_get_roles(mock_get):
+    """Test that client allows custom port."""
+    mock_response = mock.Mock(spec=Response)
+    mock_response.json.return_value = {'data': [{'some': 'data'}]}
+    mock_get.return_value = mock_response
+    response = tx_instance.get_roles('some_address')
+    assert next(response) == {'some': 'data'}
+    mock_get.assert_called_once_with(f'https://{HELIUM_API_DEFAULT_HOST}:443/v1/{base_path}/some_address/roles/',
+                                     params={})
+
+
+@mock.patch.object(Session, 'get')
+def test_get_roles_counts(mock_get):
+    """Test that client allows custom port."""
+    mock_response = mock.Mock(spec=Response)
+    mock_response.json.return_value = {'data': [{'some': 'data'}]}
+    mock_get.return_value = mock_response
+    response = tx_instance.get_roles_counts('some_address')
+    assert response[0] == {'some': 'data'}
+    mock_get.assert_called_once_with(f'https://{HELIUM_API_DEFAULT_HOST}:443/v1/{base_path}/some_address/roles/count/',
+                                     params={})
+
+
+@mock.patch.object(Session, 'get')
+def test_get_hotspot_challenges(mock_get):
+    """Test that client allows custom port."""
+    mock_response = mock.Mock(spec=Response)
+    mock_response.json.return_value = {'data': [{'some': 'data'}]}
+    mock_get.return_value = mock_response
+    response = tx_instance.get_hotspot_challenges('some_address')
+    assert next(response) == {'some': 'data'}
+    mock_get.assert_called_once_with(f'https://{HELIUM_API_DEFAULT_HOST}:443/v1/{base_path}/some_address/challenges/',
+                                     params={})
+
+
+@mock.patch.object(Session, 'get')
+def test_get_hotspot_rewards(mock_get):
+    """Test that client allows custom port."""
+    mock_response = mock.Mock(spec=Response)
+    mock_response.json.return_value = {'data': [{'some': 'data'}]}
+    mock_get.return_value = mock_response
+    response = tx_instance.get_hotspot_rewards('some_address')
+    assert next(response) == {'some': 'data'}
+    mock_get.assert_called_once_with(f'https://{HELIUM_API_DEFAULT_HOST}:443/v1/{base_path}/some_address/rewards/',
+                                     params={})
+
+
+@mock.patch.object(Session, 'get')
+def test_get_hotspot_rewards_total(mock_get):
+    """Test that client allows custom port."""
+    mock_response = mock.Mock(spec=Response)
+    mock_response.json.return_value = {'data': [{'some': 'data'}]}
+    mock_get.return_value = mock_response
+    response = tx_instance.get_hotspot_rewards_total('some_address')
+    assert response[0] == {'some': 'data'}
+    mock_get.assert_called_once_with(f'https://{HELIUM_API_DEFAULT_HOST}:443/v1/{base_path}/some_address/rewards/sum/',
+                                     params={})
+
+
+@mock.patch.object(Session, 'get')
+def test_get_hotspot_witnesses(mock_get):
+    """Test that client allows custom port."""
+    mock_response = mock.Mock(spec=Response)
+    mock_response.json.return_value = {'data': [{'some': 'data'}]}
+    mock_get.return_value = mock_response
+    response = tx_instance.get_hotspot_witnesses('some_address')
+    assert response[0] == {'some': 'data'}
+    mock_get.assert_called_once_with(f'https://{HELIUM_API_DEFAULT_HOST}:443/v1/{base_path}/some_address/witnesses/',
+                                     params={})
+
+
+@mock.patch.object(Session, 'get')
+def test_get_hotspot_witnessed(mock_get):
+    """Test that client allows custom port."""
+    mock_response = mock.Mock(spec=Response)
+    mock_response.json.return_value = {'data': [{'some': 'data'}]}
+    mock_get.return_value = mock_response
+    response = tx_instance.get_hotspot_witnessed('some_address')
+    assert response[0] == {'some': 'data'}
+    mock_get.assert_called_once_with(f'https://{HELIUM_API_DEFAULT_HOST}:443/v1/{base_path}/some_address/witnessed/',
+                                     params={})
