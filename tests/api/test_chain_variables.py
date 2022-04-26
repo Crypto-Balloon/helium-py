cdda1cfcb73b3a29c64feb283dff05f4ad2fe328
Joe <joe@cryptoballoon.net>
2022-04-26 06:58:51 -0400

    Adding unit tests for chain variables

---
+++ tests/api/test_chain_variables.py
@@ -0,0 +1,42 @@
+"""Tests for Client."""
+from unittest import mock
+
+from requests import Response, Session
+
+from helium_py.api import HELIUM_API_DEFAULT_HOST, ChainVariables
+
+tx_instance = ChainVariables()
+base_path = ChainVariables.base_path
+
+
+@mock.patch.object(Session, 'get')
+def test_get_all(mock_get):
+    """Test that client allows custom port."""
+    mock_response = mock.Mock(spec=Response)
+    mock_response.json.return_value = {'data': [{'some': 'data'}]}
+    mock_get.return_value = mock_response
+    response = tx_instance.get_all()
+    assert response[0] == {'some': 'data'}
+    mock_get.assert_called_once_with(f'https://{HELIUM_API_DEFAULT_HOST}:443/v1/{base_path}/', params={})
+
+
+@mock.patch.object(Session, 'get')
+def test_get_by_name(mock_get):
+    """Test that client allows custom port."""
+    mock_response = mock.Mock(spec=Response)
+    mock_response.json.return_value = {'data': [{'some': 'data'}, {'some': 'data'}]}
+    mock_get.return_value = mock_response
+    response = tx_instance.get_by_name('some_name')
+    assert response[0] == {'some': 'data'}
+    mock_get.assert_called_once_with(f'https://{HELIUM_API_DEFAULT_HOST}:443/v1/{base_path}/some_name/', params={})
+
+
+@mock.patch.object(Session, 'get')
+def test_all_activity(mock_get):
+    """Test that client allows custom port."""
+    mock_response = mock.Mock(spec=Response)
+    mock_response.json.return_value = {'data': [{'some': 'data'}]}
+    mock_get.return_value = mock_response
+    response = tx_instance.all_activity()
+    assert next(response) == {'some': 'data'}
+    mock_get.assert_called_once_with(f'https://{HELIUM_API_DEFAULT_HOST}:443/v1/{base_path}/activity/', params={})
